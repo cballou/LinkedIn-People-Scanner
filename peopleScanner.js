eb747fd827a1cbcbdad93353b3a115ca8ee6e830
Corey Ballou <corey@coreyballou.com>
2012-11-15 07:36:08 -0500

    Debugging a bit.

--- peopleScanner.js
+++ peopleScanner.js
@@ -24,24 +24,30 @@
         document.getElementsByTagName("head")[0].appendChild(script);
     }
 
-	// load jquery and plugin
+	/**
+     * After loading jQuery, handle plugin.
+     */
     loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js", function () {
-    var $cards = $('#people-card');
-	    if ($cards.length) {
-		// trigger initial fix
-		fixCards($cards.find('.card:not(.fixed)'));
-	
-		// watch for infinite scroll additions
-		$(document).bind('DOMNodeInserted', function(e) {
-		    var $n = $(e.target);
-		    if ($n.hasClass('card')) {
-			fixCards($cards.find('.card:not(.fixed)'));
-		    }
-		});
+        var $cardWrapper = $('#pymk-people-card');
+
+	    if ($cardWrapper.length) {
+            console.log('card wrapper found');
+
+    		// trigger initial fix
+    		fixCards($cardWrapper.find('.card:not(.fixed)'));
+    	
+    		// watch for infinite scroll additions
+    		$(document).bind('DOMNodeInserted', function(e) {
+    		    var $n = $(e.target);
+    		    if ($n.hasClass('card')) {
+    			    fixCards($cardWrapper.find('.card:not(.fixed)'));
+    		    }
+    		});
 	    }
 	
 	    function fixCards($cards) {
-	      $cards.addClass('fixed').css({ marginRight: 0, width: '100%' });
+            console.log('fixing cards');
+	        $cards.addClass('fixed').css({ marginRight: 0, width: '100%' });
 	    }
     });
 })();
